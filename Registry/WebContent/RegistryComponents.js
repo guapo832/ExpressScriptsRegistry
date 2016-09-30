a8683b28a874a14dbbe6bedd3ba46087a6e2a82b
guapo832 <BXBA0K@EALTHWPFK72LT.gbcmetals.net>
2016-09-29 20:37:13 -0500

    removed reference to a javascript file.

--- Registry/WebContent/RegistryComponents.js
+++ Registry/WebContent/RegistryComponents.js
@@ -85,7 +85,14 @@ var CreateModal = React.createClass({
 		this.loadCreateForm(this.props.scope);
 	},	
 
-
+    handleChange:function(event){
+    	alert('test');
+    },
+    
+    handleClick:function(event){
+    	alert('test');
+    },
+    
 	loadCreateForm: function(scopeId) {
 	 	
 	
@@ -96,7 +103,7 @@ var CreateModal = React.createClass({
 	            	confirm:{
 	            		label:'Save',
 	            		className:'btn',
-	            		callback:function(result){alert('testing');}
+	            		callback:function(result){alert($("#exampleInputEmail1").val())}
 	            	},
 	            	cancel:{
 	            		label:'Cancel',
@@ -112,83 +119,15 @@ var CreateModal = React.createClass({
 	},	
 	render: function() {
 	
-	var hdnModal={
-		display:'none'
-	}
+	
 	
 	return (
 	<div>
-		<div style={hdnModal} id="hdnModal">
-		 <input type="button" className="btn btn-primary" value="test"/>
-		<div className="form-group">
-	    <label for="exampleInputEmail1">Email address</label>
-	    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-	    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-	  </div>
-	  <div className="form-group">
-	    <label for="exampleInputPassword1">Password</label>
-	    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-	  </div>
-	  <div className="form-group">
-	    <label for="exampleSelect1">Example select</label>
-	    <select className="form-control" id="exampleSelect1">
-	      <option>1</option>
-	      <option>2</option>
-	      <option>3</option>
-	      <option>4</option>
-	      <option>5</option>
-	    </select>
-	  </div>
-	  <div className="form-group">
-	    <label for="exampleSelect2">Example multiple select</label>
-	    <select multiple class="form-control" id="exampleSelect2">
-	      <option>1</option>
-	      <option>2</option>
-	      <option>3</option>
-	      <option>4</option>
-	      <option>5</option>
-	    </select>
-	  </div>
-	  <div class="form-group">
-	    <label for="exampleTextarea">Example textarea</label>
-	    <textarea className="form-control" id="exampleTextarea" rows="3"></textarea>
-	  </div>
-	  <div class="form-group">
-	    <label for="exampleInputFile">File input</label>
-	    <input type="file" className="form-control-file" id="exampleInputFile" aria-describedby="fileHelp"/>
-	    <small id="fileHelp" className="form-text text-muted">This is some placeholder block-level help text for the above input. It's a bit lighter and easily wraps to a new line.</small>
-	  </div>
-	  <fieldset class="form-group">
-	    <legend>Radio buttons</legend>
-	    <div class="form-check">
-	      <label class="form-check-label">
-	        <input type="radio" className="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked/>
-	        Option one is this and that&mdash;be sure to include why it's great
-	      </label>
-	    </div>
-	    <div className="form-check">
-	    <label className="form-check-label">
-	        <input type="radio" className="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2"/>
-	        Option two can be something else and selecting it will deselect option one
-	      </label>
-	    </div>
-	    <div className="form-check disabled">
-	    <label className="form-check-label">
-	        <input type="radio" className="form-check-input" name="optionsRadios" id="optionsRadios3" value="option3" disabled/>
-	        Option three is disabled
-	      </label>
-	    </div>
-	  </fieldset>
-	  <div class="form-check">
-	    <label class="form-check-label">
-	      <input type="checkbox" className="form-check-input"/>
-	      Check me out
-	    </label>
-	  </div>
-
-		
-		
-       </div>
+		<div id="hdnModal" style={closeStyle}>
+		   <div id="container" style={containerStyle}>
+		 <div type="button" className="btn btn-primary" onClick={this.handleClick}>rest</div>
+		</div>
+		</div>
 	   <button type="button" onClick={this.handleCreateClick} className="btn">create</button>
 	</div>);
 
